67a229c19870a3014e6c404c8976bed7a5386521
trankhanh040147 <trankhanh040147@gmail.com>
2026-01-04 15:07:19 +0700

    talwind plugin

--- solar-satellite/astro.config.mjs
+++ solar-satellite/astro.config.mjs
@@ -2,8 +2,16 @@
 import { defineConfig } from 'astro/config';
 
 import mdx from '@astrojs/mdx';
+import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [mdx()]
+  integrations: [mdx()],
+  vite: {
+    plugins: [
+      tailwindcss({
+        plugins: ['@tailwindcss/typography'],
+      })
+    ],
+  },
 });
